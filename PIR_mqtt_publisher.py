8edf6b9761f07243418a049a81b16dffe8201c97
Tignileef <skez665@m365.dongyang.ac.kr>
2025-06-05 16:16:35 +0900

    Update PIR_mqtt_publisher.py
    update PIR

--- PIR_mqtt_publisher.py
+++ PIR_mqtt_publisher.py
@@ -1,12 +1,28 @@
 # mqtt_publisher.py
 import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
 import time
 
-client = mqtt.Client()
-client.connect("127.0.0.1", 1883, 60)
+PIR_PIN = 17
+LED_PIN = 13
 
-while True:
-	time.sleep(15)
-	client.publish("sensor/motion", "motion_detected")
-	print("MQTT message send")
-	time.sleep(600)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIR_PIN, GPIO.IN)
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+client = mqtt.Client()
+client.connect("223.194.166.218", 1883, 60)
+
+try:
+	while True:
+		if GPIO.input(PIR_PIN):
+			GPIO.output(LED_PIN, GPIO.HIGH)
+			client.publish("sensor/motion", "motion_detected")
+			print("MQTT message send")
+			time.sleep(10)
+	
+except KeyboardInterrupt:
+	print("프로그램 종료")
+
+finally:
+    GPIO.cleanup()
